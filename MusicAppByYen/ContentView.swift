13f470bb74f58e054f0121f4031957e03d69e71b
Yen <pnf4665jds@gmail.com>
2020-07-21 09:35:58 -0700

    Initial Commit

---
+++ MusicAppByYen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicAppByYen
+//
+//  Created by YenChang on 2020/7/21.
+//  Copyright © 2020 YenChang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
